527d194b70b52c89c800e835e25a61bd588ddb17
Henrique Barbosa de Souza Júnior <henrique@hbveterinaria.com.br>
2025-04-01 08:35:29 -0300

    Alteração de msg de porta

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(routes);
 
 
 app.listen(process.env.PORT ?? 3344, 
-    ()=> process.env.PORT ? console.log(process.env.PORT) : 
+    ()=> process.env.PORT ? console.log('Porta do servidor :' + process.env.PORT) : 
                             console.log('Servidor rodando na porta 3344'));
     
      
